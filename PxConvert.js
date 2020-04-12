3454f170d93e3b9fbc7b40fdc530c0ca59b9529e
Fábio <47451463+FabioSmuu@users.noreply.github.com>
2020-04-12 02:07:56 -0300

    Update PxConvert.js

--- PxConvert.js
+++ PxConvert.js
@@ -6,5 +6,11 @@ function emTOpx(em = 0, base = 19.20) {
 	return (isNaN (em) || isNaN (base)) ? 'NaN' : em*base + "px"
 }
 
+/* Converte px para %
+function pxTOper(px = 0, tela = 1024) {
+	return (isNaN (px) || isNaN (base)) ? 'NaN' : px*100/tela + "em"
+}
+*/
+
 pxTOem(240) //Converte 240px para 12.5em
 emTOpx(12.5) //Converte 12.5em para 240px
