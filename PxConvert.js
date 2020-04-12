fea9287192af068723fdd86d5f88651f8e8152ca
Fábio <47451463+FabioSmuu@users.noreply.github.com>
2020-04-12 10:23:21 -0300

    Update PxConvert.js

--- PxConvert.js
+++ PxConvert.js
@@ -1,19 +1,39 @@
-function pxTOem(px = 0, base = 19.20) { //base padrão 16
-	return (isNaN (px) || isNaN (base)) ? 'NaN' : px/base + "em"
+//Para NodeJS
+const {createInterface} = require('readline')
+
+let cmd = createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+console.log([
+	'\n Digite um comando para conversão:',
+	' # Lista de comandos:\n',
+	'   ?px 16	- Converte ?px para ?em de base 16 (Padrão)',
+	'   ?em 16	- Converte ?em para ?px de base 16 (Padrão)',
+	'\n Exemplo:\n   240px 16\n',
+].join('\n'))
+
+cmd.question(' # Comando: ', comando => {
+	comando = comando.split(' ')
+	
+	comando = (comando[0].toLowerCase().endsWith('px')) ? pxTOem(comando[0].replace(/px/gi, ''), comando[1]) :
+	(comando[0].toLowerCase().endsWith('em')) ? emTOpx(comando[0].replace(/em/gi, ''), comando[1]) : '---'
+	
+	console.log('\n	Resultado:	'+ comando + '\n')
+	cmd.close()
+})
+
+function pxTOem(px = 0, base = 16) {
+	return (isNaN (px) || isNaN (base)) ? '---' : px/base + "em"
 }
 
-function emTOpx(em = 0, base = 19.20) { //base padrão 16
-	return (isNaN (em) || isNaN (base)) ? 'NaN' : em*base + "px"
+function emTOpx(em = 0, base = 16) {
+	return (isNaN (em) || isNaN (base)) ? '---' : em*base + "px"
 }
 
-/* Converte px para %
+/* Em andamento....
 function pxTOper(px = 0, tela = 1024) {
 	return (isNaN (px) || isNaN (base)) ? 'NaN' : px*100/tela + "em"
 }
-
-pxTOper(240) //Converte 240px para 23.4375%
-pxTOper(240, 1359) //Converte 240px para 17.660044150110377%
 */
-
-pxTOem(240) //Converte 240px para 12.5em
-emTOpx(12.5) //Converte 12.5em para 240px
